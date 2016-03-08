49ef52ad82eb99aec3d8c1fcbcb044b9f5830515
jansanchez <joejansanchez@gmail.com>
2016-03-08 15:48:03 -0500

    refacotr

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const escomplex = require('escomplex');
 const vinyl = require('vinyl-fs');
 const map = require('map-stream');
 const Symbol = require('./symbol');
-const s = new Symbol();
+const symbols = new Symbol();
 
 class Complex {
 	constructor(path) {
@@ -21,7 +21,25 @@ class Complex {
 			noCoreSize: false,
 			maintainability: 171
 		};
+		this._avg = {
+			low: {
+				value: 0,
+				color: 'red'
+			},
+			medium: {
+				value: 0.5,
+				color: 'yellow'
+			},
+			high: {
+				value: 0.75,
+				color: 'green'
+			}
+		};
 		this._path = path;
+	}
+
+	get avg() {
+		return this._avg;
 	}
 
 	get options() {
@@ -44,18 +62,18 @@ class Complex {
 
 	generator(score, threshold) {
 		const magnitude = Math.floor(score / (this.options.maintainability / 10));
-		const bar = `${s.getSymbol('bullet').repeat(magnitude / 2)}  ${score.toFixed(2)}`;
+		const bar = `${symbols.getSymbol('bullet').repeat(magnitude / 2)}  ${score.toFixed(2)}`;
 		const rating = score / threshold;
 
-		let color = 'red';
-		let arrow = s.getSymbol('downwards');
+		let color = this.avg.low.color;
+		let arrow = symbols.getSymbol('downwards');
 
-		if (rating >= 0.50) {
-			color = 'yellow';
+		if (rating >= this.avg.medium.value) {
+			color = this.avg.medium.color;
 		}
-		if (rating >= 0.80) {
-			arrow = s.getSymbol('upwards');
-			color = 'green';
+		if (rating >= this.avg.high.value) {
+			color = this.avg.high.color;
+			arrow = symbols.getSymbol('upwards');
 		}
 
 		return {
@@ -75,14 +93,14 @@ class Complex {
 				const result = escomplex.analyse([{path: data.relative, code: newCode}], this.options);
 				const values = this.generator(result.reports[0].maintainability, this.options.maintainability);
 
-				console.log(color[values.color](`${values.symbol}  ${result.reports[0].path}  ${values.bar}`));
+				console.log(color[values.color](` ${values.symbol}  ${result.reports[0].path}  ${values.bar}`));
 
 				if (values.color === 'red') {
 					result.reports[0].functions.forEach(fn => {
-						console.log(color.white.dim(`   ${s.getSymbol('rightwards')} line: ${fn.line}, method: ${fn.name}, cyclomatic: ${fn.cyclomatic}, effort: ${fn.halstead.effort.toFixed(2)}, vocabulary: ${fn.halstead.vocabulary}`));
+						console.log(color.white.dim(`    ${symbols.getSymbol('rightwards')} line: ${fn.line}, method: ${fn.name}, cyclomatic: ${fn.cyclomatic}, effort: ${fn.halstead.effort.toFixed(2)}, vocabulary: ${fn.halstead.vocabulary}`));
 					});
 					if (result.reports[0].functions.length === 0) {
-						console.log(color.white.dim(`   ${s.getSymbol('rightwards')} line: ${result.reports[0].aggregate.line}, method: ${result.reports[0].aggregate.name}, cyclomatic: ${result.reports[0].aggregate.cyclomatic}, effort: ${result.reports[0].aggregate.halstead.effort.toFixed(2)}, vocabulary: ${result.reports[0].aggregate.halstead.vocabulary}`));
+						console.log(color.white.dim(`    ${symbols.getSymbol('rightwards')} line: ${result.reports[0].aggregate.line}, method: ${result.reports[0].aggregate.name}, cyclomatic: ${result.reports[0].aggregate.cyclomatic}, effort: ${result.reports[0].aggregate.halstead.effort.toFixed(2)}, vocabulary: ${result.reports[0].aggregate.halstead.vocabulary}`));
 					}
 				}
 				callback(null, data);
