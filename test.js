27f578c13697f228f6237aa20a9ae135fff377f6
Jan Sanchez <joejansanchez@gmail.com>
2016-03-12 21:35:02 -0500

    test(jscomplex): agrego 12 pruebas unitarias

--- test.js
+++ test.js
@@ -4,18 +4,58 @@
 /*
 test of jscomplex
 @author Jan Sanchez
- */
+*/
 
 import test from 'ava';
 const Complex = require('./lib/jscomplex');
 
-test.skip(t => {
-    t.same([1, 2], [1, 2]);
-});
-
-const complex = new Complex(['./*.js']);
+const complex = new Complex(null, {maintainability: 200});
 complex.process();
 
-test('version exists', t => {
-	t.true(true);
+test('Options: format', t => {
+	t.same(complex.options.format, 'json');
+});
+
+test('Options: logicalor', t => {
+	t.true(complex.options.logicalor);
+});
+
+test('Options: switchcase', t => {
+	t.true(complex.options.switchcase);
+});
+
+test('Options: forin', t => {
+	t.false(complex.options.forin);
+});
+
+test('Options: trycatch', t => {
+	t.false(complex.options.trycatch);
+});
+
+test('Options: newmi', t => {
+	t.false(complex.options.newmi);
+});
+
+test('Options: newmi', t => {
+	t.false(complex.options.newmi);
+});
+
+test('Options: ignoreErrors', t => {
+	t.false(complex.options.ignoreErrors);
+});
+
+test('Options: noCoreSize', t => {
+	t.false(complex.options.noCoreSize);
+});
+
+test('Options: maintainability is equal to 200', t => {
+	t.same(complex.options.maintainability, 200);
+});
+
+test('Path is Array', t => {
+	t.true(Array.isArray(complex.path));
+});
+
+test('Path[0] is equal to "./**/*.js"', t => {
+	t.same(complex.path[0], './**/*.js');
 });
